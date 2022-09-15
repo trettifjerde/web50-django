e675f5f262bcf0f8f7ac31bd6704651b86902059
trettifjerde <trettifjerde@yandex.ru>
2022-09-15 18:46:11 +0200

    mobile img toggle done

--- home/static/home/home.js
+++ home/static/home/home.js
@@ -3,6 +3,7 @@ const timerDelay = 7000;
 
 let currentProjectI;
 let currentProject;
+let currentImgI;
 let currentImg;
 let timerId = null;
 let projectLocked = false;
@@ -23,25 +24,28 @@ $(function(){
 
         $(".project").each(function(i) {
             $(this).on("click", () => toggleMobileDescription(i));
+            $(this).find('.project-img-btns button').each(function(j) {
+                $(this).on("click", ()=> showImg(j));
+            });
         });
     }
 });
 
 function toggleGallery() {
-    let nextImg = currentImg.nextElementSibling;
-    if (! nextImg) {
-        if (! projectLocked){
+    let nextImgI;
+    if (! currentImg.nextElementSibling)
+    {
+        if (!projectLocked) {
             toggleProjects();
             return;
         }
-        else 
-            nextImg = currentProject.querySelector('.project-img');
+        else
+            nextImgI = 0;
     }
+    else
+        nextImgI = currentImgI + 1;
 
-    const previousImg = currentImg;
-    setCurrentImg(nextImg);
-
-    $(previousImg).fadeOut(fadeDuration, ()=> $(currentImg).fadeIn(fadeDuration));
+    toggleImgs(nextImgI);
 }
 
 function setCurrentProject(i) {
@@ -53,12 +57,22 @@ function setCurrentProject(i) {
         currentProjectI = 0;
 
     currentProject = $(`.project:eq(${currentProjectI})`)[0];
-    setCurrentImg(currentProject.querySelector('.project-img'));
+    setCurrentImg(0);
 }
 
-function setCurrentImg(img) {
-    currentImg = img;
-    loadLazy(img);
+function setCurrentImg(i) {
+
+    currentImgI = i;
+    currentImg = currentProject.querySelectorAll('.project-img')[i];
+    loadLazy(currentImg);
+    $(currentProject).find('.img-btn-active').removeClass('img-btn-active');
+    currentProject.querySelectorAll('.project-img-btns button')[i].classList.add('img-btn-active');
+}
+
+function toggleImgs(i) {
+    const previousImg = currentImg;
+    setCurrentImg(i);
+    $(previousImg).fadeOut(fadeDuration, ()=> $(currentImg).fadeIn(fadeDuration));
 }
 
 function loadLazy(imgDiv) {
@@ -100,6 +114,15 @@ function showProject(i) {
         toggleProjects(i);
 }
 
+function showImg(i) {
+    event.stopPropagation();
+    if (i !== currentImgI) {
+        toggleImgs(i);
+        clearInterval(timerId);
+        timerId = setInterval(toggleGallery, timerDelay);
+    }
+}
+
 function lockProject(i){
     const li = $(`.sidebar li:eq(${i})`);
 
@@ -121,9 +144,14 @@ function unlockProject() {
 
 function toggleMobileDescription(i) {
     if (window.innerHeight > window.innerWidth) {
-        event.stopPropagation();
-        currentProject.querySelector('.project-description').classList.toggle('visible');
+        const desc = currentProject.querySelector('.project-description');
+        if (desc.classList.contains('visible'))
+            desc.classList.remove('visible');
+        else {
+            desc.classList.add('visible');
+            lockProject(i);
+            event.stopPropagation();
+        }
     }
-
-    if (!projectLocked) lockProject(i);
+    else if (!projectLocked) lockProject(i);
 }
