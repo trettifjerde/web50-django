c0a2ed1481ac4ae9ed5e971a347e482d932eb01d
trettifjerde <trettifjerde@yandex.ru>
2022-10-26 23:30:38 +0200

    network: post sliced into components, post toggle animations done

--- network/static/network/feed.js
+++ network/static/network/feed.js
@@ -2,7 +2,7 @@ let handlePostScroll;
 const app = Vue.createApp({
     template: `<new-post-form v-if="isNewPostFormVisible()" @post="addNewPost"/> 
     <div ref="mainErrorMsg" v-show="errorMsg" class="error-msg">{{ errorMsg }}</div>
-    <post v-for="post in feedPosts" :post="post" :key="post.id" @update="updatePost" @error="showError"/>
+    <post v-for="post in feedPosts" :post="post" :key="post.id" @update="handleUpdate"/>
     <div v-show="loadStatusMsg" class="c">{{ loadStatusMsg }}</div>`,
     data() {
         return {
@@ -24,14 +24,14 @@ const app = Vue.createApp({
         }
     },
     methods: {
-        async getMorePosts() {
+        getMorePosts() {
             this.currentPage++;
             this.pending = true;
             this.loadStatusMsg = 'Loading...';
 
             const url = `/network/morePosts/?page=${this.currentPage}&feedType=${this.feedType}`;
 
-            return await fetch(url, {
+            fetch(url, {
                 headers: {'X-Requested-With': 'XMLHttpRequest'}
             })
             .then(res => res.json())
@@ -56,6 +56,15 @@ const app = Vue.createApp({
                     this.errorMsg = 'Problem loading posts. Try again later.';
             })
             .catch(err => console.log(err))
+        },
+        handleUpdate(updateType, info) {
+            switch(updateType) {
+                case 'error':
+                    this.errorMsg = info.error;
+                    break;
+                default:
+                    this.updatePost(updateType, info);
+            }
         },
         updatePost(eventType, info) {
             const i = this.feedPosts.findIndex(p => p.id === info.id);
@@ -88,9 +97,6 @@ const app = Vue.createApp({
                 if (! this.pending) this.getMorePosts();
             }
         },
-        showError(msg) {
-            this.errorMsg = msg;
-        }
     },
     async created() {
         handlePostScroll = this.handlePostScroll.bind(this);
@@ -100,81 +106,34 @@ const app = Vue.createApp({
 
 });
 
-app.component('post', {
-    template: `<div class="post">
-    <div v-show="!editFormVisible && !deleteConfirmationVisible" class="post-post">
-        <div class="post-header">
-            <div class="post-avatar">
-                <img :src="post.avatar">
-            </div>
-            <div>
-                <p class="b"><a :href="getNetworkerUrl()">{{ post.networker }}</a></p>
-                <div class="extra">
-                    <span>
-                        <span class="b">Created: </span>{{ post.created }}
-                    </span>
-                    <span v-if="post.created !== post.edited">
-                        <span class="b">Edited: </span>
-                        <span class="last-edited">{{ post.edited }}</span> 
-                    </span>
-                </div>
-            </div>
-            <div class="spacer"></div>     
-            <button v-if="post.self" class="btn-sm" @click="togglePostEditForm()">Edit</button>
-            <button v-if="post.self" class="btn-sm" @click="toggleDeletePost()">Delete</button>
-        </div>
-        <div class="post-body" v-html="post.text"></div>
-        <div class="post-footer">
-            <button v-if="getSignedStatus()" class="btn btn-likes" @click="like()">
-                <div :class="getLikeClass()"></div>
-                <span class="num-likes">{{ post.likes }}</span>
-            </button>
-            <button v-else class="btn btn-likes" disabled title="Sign in to leave likes">
-                <div class="heart"></div>
-                <span class="num-likes">{{ post.likes }}</span>
-            </button>
-        </div>
-    </div>
-    <div v-if="post.self" v-show="editFormVisible || deleteConfirmationVisible" class="post-meta">
-        <form v-show="editFormVisible">
-            <p v-show="editFormErrorMessage" class="error-msg">{{ editFormErrorMessage }}</p>
-            <textarea name="text" v-model="editedPostText" required></textarea>
-            <button type="submit" @click.prevent="savePost()">Save</button>
-            <button type="button" @click="togglePostEditForm()">Cancel</button>
-        </form>
-        <div v-show="deleteConfirmationVisible" class="post-meta">
-            <div class="post-meta-msg">Are you sure you want to delete this post?</div>
-            <button type="button" @click="deletePost()">Confirm</button>
-            <button type="button" @click="toggleDeletePost()">Cancel</button>
-        </div>
+app.component('post-meta', {
+    template: `<div class="post-meta">
+    <form v-show="editVisible">
+        <p v-show="editFormErrorMessage" class="error-msg">{{ editFormErrorMessage }}</p>
+        <textarea name="text" v-model="editedPostText" required></textarea>
+        <button type="submit" @click.prevent="savePost()">Save</button>
+        <button type="button" @click="emitToggle('edit')">Cancel</button>
+    </form>
+    <div v-show="deleteVisible" class="post-meta">
+        <div class="post-meta-msg">Are you sure you want to delete this post?</div>
+        <button type="button" @click="deletePost()">Confirm</button>
+        <button type="button" @click="emitToggle('delete')">Cancel</button>
     </div>
 </div>`,
+    props: ['post', 'editVisible', 'deleteVisible'],
     data() {
         return {
-            editedPostText: '',
-            editFormVisible: false,
             editFormErrorMessage: '',
-            deleteConfirmationVisible: false,
+            editedPostText: '',
         }
     },
-    props: ['post'],
+    mounted() {
+        this.editFormErrorMessage = '';
+        this.editedPostText = this.post.text;
+    },
     methods: {
-        getLikeClass() {
-            return `heart ${this.post.liked ? 'liked' : ''}`; 
-        },
-        getSignedStatus() {
-            return signedIn;
-        },
-        getNetworkerUrl() {
-            return `/network/user/${this.post.networkerId}/`;
-        },
-        togglePostEditForm() {
-            this.editedPostText = this.post.text;
-            this.editFormVisible = ! this.editFormVisible;
-            this.editFormErrorMessage = '';
-        },
-        toggleDeletePost() {
-            this.deleteConfirmationVisible = !this.deleteConfirmationVisible;
+        emitToggle(btn) {
+            this.$emit('toggle', btn);
         },
         savePost() {
             if (this.editedPostText === this.post.text)
@@ -202,7 +161,7 @@ app.component('post', {
         },
         updatePost(post) {
             this.$emit('update', 'edit', post);
-            this.togglePostEditForm();
+            this.$emit('toggle', 'edit');
         },
         deletePost() {
             fetch('/network/deletePost/', {
@@ -212,11 +171,56 @@ app.component('post', {
             })
             .then(res => res.json())
             .then(data => {      
-                if ('error' in data) this.$emit('error', data.error);
-                else if ('ok' in data) this.$emit('update', 'remove', {id:this.post.id});
+                if ('error' in data) this.$emit('update', 'error', {'error': data.error});
+                else if ('ok' in data) this.$emit('update', 'remove', {id:this.post.id });
                 else throw data;
             })
             .catch(err => console.log(err));
+        },
+    },
+});
+
+app.component('post-post', {
+    template: `<div class="post-post">
+    <div class="post-header">
+        <div class="post-avatar">
+            <img :src="post.avatar">
+        </div>
+        <div>
+            <p class="b"><a :href="getNetworkerUrl()">{{ post.networker }}</a></p>
+            <div class="extra">
+                <span>
+                    <span class="b">Created: </span>{{ post.created }}
+                </span>
+                <span v-if="post.created !== post.edited">
+                    <span class="b">Edited: </span>
+                    <span class="last-edited">{{ post.edited }}</span> 
+                </span>
+            </div>
+        </div>
+        <div class="spacer"></div>     
+        <button v-if="post.self" class="btn-sm" @click="emitToggle('edit')">Edit</button>
+        <button v-if="post.self" class="btn-sm" @click="emitToggle('delete')">Delete</button>
+    </div>
+    <div class="post-body" v-html="post.text"></div>
+    <div class="post-footer">
+        <button v-if="isSignedIn" class="btn btn-likes" @click="like()">
+            <div :class="{heart: true, liked: post.liked}"></div>
+            <span class="num-likes">{{ post.likes }}</span>
+        </button>
+        <button v-else class="btn btn-likes" disabled title="Sign in to leave likes">
+            <div class="heart"></div>
+            <span class="num-likes">{{ post.likes }}</span>
+        </button>
+    </div>
+</div>`,
+    props: ['post', 'isSignedIn'],
+    methods: {
+        getNetworkerUrl() {
+            return `/network/user/${this.post.networkerId}/`;
+        },
+        emitToggle(btn) {
+            this.$emit('toggle', btn);
         },
         like() {
             fetch('/network/like/', {
@@ -237,6 +241,59 @@ app.component('post', {
                 else throw data;
             })
             .catch(err => console.log(err));
+        }
+    },
+
+});
+
+app.component('my-post', {
+    template: `<transition name="post" mode="out-in">
+    <post-post v-if="!metaVisible" :isSignedIn="true" :post="post" @toggle="toggle" @update="emitUpdate" />
+    <post-meta v-else :post="post" :editVisible="editFormVisible" :deleteVisible="deleteConfirmationVisible" 
+    @toggle="toggle" @update="emitUpdate" />
+</transition>`,
+    components: ['post-post', 'post-meta'],
+    props: ['post'],
+    data() {
+        return {
+            editFormVisible: false,
+            deleteConfirmationVisible: false,
+        }
+    },
+    computed: {
+        metaVisible() {
+            return this.editFormVisible || this.deleteConfirmationVisible
+        }
+    },
+    methods: {
+        toggle(btnName) { 
+            switch(btnName) {
+                case 'edit':
+                    this.editFormVisible = ! this.editFormVisible;
+                    break;
+                case 'delete':
+                    this.deleteConfirmationVisible = !this.deleteConfirmationVisible;
+                    break;
+            }       
+        },
+        emitUpdate(updateName, info) {
+            this.$emit('update', updateName, info);
+        }
+    }
+});
+
+app.component('post', {
+    template: `<div class="post">
+    <my-post v-if="post.self" :post="post" @update="emitUpdate" />
+    <post-post v-else :isSignedIn="isSignedIn()" :post="post" @update="emitUpdate" />
+</div>`,
+    props: ['post'],
+    methods: {
+        isSignedIn() {
+            return signedIn;
+        },
+        emitUpdate(updateName, info) {
+            this.$emit('update', updateName, info);
         }
     }
 });
